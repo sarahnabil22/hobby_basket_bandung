26d2663c62c1b5f0e6779b70ef4efe0d5241c46e
wills <williams.gunawan123@gmail.com>
2022-12-06 21:07:54 +0700

    Enhance design and responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,28 +22,15 @@
             <h1>EVENTS</h1> 
         </div>
     </div>
-    <div class="row justify-content-center">
-        <div class="col-4">
-            <div class="border-1 shadow bg-primary">
-                <div>
-                    basketball tournament
+    <div class="row justify-content-center m-auto" style="max-width: 900px;">
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div class="p-3 border-1 shadow bg-primary text-light" style="text-align-last: center;">
+                <img src="/img/poster.jpg" class="w-100"/>
+                <div class="fw-bold">
+                    <u>BASKETBALL TOURNAMENT</u>
                 </div>
                 <div>
-                    keterangan singkat
-                </div>
-                <div>
-                    <a name="" id="" class="btn btn-light" href="#" role="button">More Info</a>
-                </div>
-            </div>
-
-        </div>
-        <div class="col-4">
-            <div class="border-1 shadow bg-primary">
-                <div>
-                    basketball tournament
-                </div>
-                <div>
-                    keterangan singkat
+                    Keterangan Singkat
                 </div>
                 <div>
                     <a name="" id="" class="btn btn-light" href="#" role="button">More Info</a>
@@ -51,13 +38,29 @@
             </div>
 
         </div>
-        <div class="col-4">
-            <div class="border-1 shadow bg-primary">
-                <div>
-                    basketball tournament
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div class="p-3 border-1 shadow bg-primary text-light" style="text-align-last: center;">
+                <img src="/img/poster.jpg" class="w-100"/>
+                <div class="fw-bold">
+                    <u>BASKETBALL TOURNAMENT</u>
                 </div>
                 <div>
-                    keterangan singkat
+                    Keterangan Singkat
+                </div>
+                <div>
+                    <a name="" id="" class="btn btn-light" href="#" role="button">More Info</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div class="p-3 border-1 shadow bg-primary text-light" style="text-align-last: center;">
+                <img src="/img/poster.jpg" class="w-100"/>
+                <div class="fw-bold">
+                    <u>BASKETBALL TOURNAMENT</u>
+                </div>
+                <div>
+                    Keterangan Singkat
                 </div>
                 <div>
                     <a name="" id="" class="btn btn-light" href="#" role="button">More Info</a>
