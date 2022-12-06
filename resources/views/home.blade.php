59b4423f3a99dbeedede4852844a531d6f3026b3
wills <williams.gunawan123@gmail.com>
2022-12-06 20:48:16 +0700

    Initialize Home Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid p-0">
+    <div class="d-flex">
+        <div style="
+            background-image: url(/img/basket-bg.jpg);
+            width: -webkit-fill-available;
+            height: 40vh;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+            ">
+
+        </div>
+    
+    </div>
+</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 text-center">
+            <h1>EVENTS</h1> 
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <div class="border-1 shadow bg-primary">
+                <div>
+                    basketball tournament
+                </div>
+                <div>
+                    keterangan singkat
+                </div>
+                <div>
+                    <a name="" id="" class="btn btn-light" href="#" role="button">More Info</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-4">
+            <div class="border-1 shadow bg-primary">
+                <div>
+                    basketball tournament
+                </div>
+                <div>
+                    keterangan singkat
+                </div>
+                <div>
+                    <a name="" id="" class="btn btn-light" href="#" role="button">More Info</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-4">
+            <div class="border-1 shadow bg-primary">
+                <div>
+                    basketball tournament
+                </div>
+                <div>
+                    keterangan singkat
+                </div>
+                <div>
+                    <a name="" id="" class="btn btn-light" href="#" role="button">More Info</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
